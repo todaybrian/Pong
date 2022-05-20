42b69682dab309a9b0d1999a04054ebb535acba4
todaybrian <todaybryan@gmail.com>
2022-05-19 23:48:02 -0400

    e

--- src/todaybrian/GameFrame.java
+++ src/todaybrian/GameFrame.java
@@ -9,15 +9,17 @@ public class GameFrame extends JFrame {
 
     public GameFrame() {
         gamePanel = new GamePanel();
+
         add(gamePanel);
-        setTitle("Pong!");
+        setTitle("Pong!"); //set title for frame
+        setResizable(false); //frame can't change size
         setBackground(Color.BLACK);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        pack();
-        setVisible(true);
-        setResizable(false);
-        setLocationRelativeTo(null);
-        requestFocus();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //X button will stop program execution
+        pack(); //makes components fit in window - don't need to set JFrame size, as it will adjust accordingly
+        setVisible(true); //makes window visible to user
+        setLocationRelativeTo(null);//set window in middle of screen
+
+        requestFocus(); //Make window the active window
     }
 
 
